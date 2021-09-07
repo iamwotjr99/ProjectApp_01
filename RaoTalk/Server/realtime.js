50d2615285d5fd268e65efa52f6d495ab506322d
iamwotjr99 <iamwotjr@gmail.com>
2021-09-07 20:47:30 +0900

    [추가] 소켓서버 채팅 기능

--- RaoTalk/Server/realtime.js
+++ RaoTalk/Server/realtime.js
@@ -1,21 +1,32 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+var express = require('express');
+var socket = require('socket.io');
 
-let clients = [];
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(3000);
+
+var io = socket(server);
 
 io.on('connection', (socket) => {
-    clients.push(socket);
-    console.log('a user connected');
+    console.log('made socket connection', socket.id);
 
-    socket.on('send', (data) => {
-        console.log("받은 메세지: " + data.message);
-        data.message = "Message from Server : " + data.message;
-        socket.emit('send', data)
+    socket.on('chat', (data) => {
+        console.log(data);
+        io.socket.emit('chat', data);
+    });
+
+    // socket.on('typing', (data) => {
+    //     socket.broadcast.emit('typing', data);
+    // })
+
+    // socket.on('stop typing', (data) => {
+    //     socket.broadcast.emit('stop typing', data);
+    // });
+
+    socket.on('new message', (data) => {
+        console.log(data);
+        socket.broadcast.emit('new message', data);
     })
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        let i = clients.indexOf(socket);
-        clients.splice(i, 1);
-    })
-});
+})
