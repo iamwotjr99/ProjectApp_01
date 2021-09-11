69630cfb61246739b0310914292d9d1ed90097a8
studycode167 <77335708+studycode167@users.noreply.github.com>
2021-09-11 23:07:09 +0900

    [추가]mybatis

--- RaoTalk/Server/DB/mybatis.js
+++ RaoTalk/Server/DB/mybatis.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');  //My-sql을 사용하였다.
 const mybatisMapper = require('mybatis-mapper');  //매핑할 마이바티스
 
 const connection = mysql.createConnection({  //커넥션 생성
+    
     host: 'raotalk.cn4z0cetscin.ap-northeast-2.rds.amazonaws.com',
     user: 'fisixeven',
     database: 'TESTDB',
@@ -9,7 +10,7 @@ const connection = mysql.createConnection({  //커넥션 생성
 });
 
 
-mybatisMapper.createMapper(['DB:/chat_message.xml']);  //예) xml파일이 D드라이브에 있다면, D:/매퍼.xml
+mybatisMapper.createMapper(['C:/GitHub/ProjectApp_01/RaoTalk/Server/DB/chat_message.xml']);  //예) xml파일이 D드라이브에 있다면, D:/매퍼.xml
 
 //조회할 파라미터
 var param = {
@@ -18,7 +19,7 @@ var param = {
 
 //질의문 형식
 var format = {language: 'sql', indent: '  '};
-var query = mybatisMapper.getStatement('testMapper', 'testBasic', param, format);
+var query = mybatisMapper.getStatement('chat_message', 'selectviewmsg', param, format);
 //첫번째는 xml의 name값, 두번째는 해당 xml의 id값, 세번째는 파라미터, 마지막은 포맷이다.
 
 console.log(query);  //해당쿼리가 조합된 것을 볼 수 있다.
