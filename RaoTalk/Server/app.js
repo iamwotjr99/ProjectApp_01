19c9b9f1de456400855a65588cd5bf776bade68b
younglee-Choe <leele7111@naver.com>
2021-09-29 21:26:00 +0900

    [수정] app.js

--- RaoTalk/Server/app.js
+++ RaoTalk/Server/app.js
@@ -4,16 +4,12 @@ const router = require('./router');
 const app = express();
 
 app.use(morgan('dev'));
-<<<<<<< HEAD
-app.use('/qgis2web1', express.static(__dirname + '/qgis2web_oneThousand'));
-=======
 app.use('/qgis2web0', express.static(__dirname + '/qgis2web_total'));
 app.use('/qgis2web1', express.static(__dirname + '/qgis2web_oneThousand'));
 app.use('/qgis2web2', express.static(__dirname + '/qgis2web_twoThousand'));
 app.use('/qgis2web3', express.static(__dirname + '/qgis2web_threeThousand'));
 app.use('/qgis2web4', express.static(__dirname + '/qgis2web_fourThousand'));
 app.use('/qgis2web5', express.static(__dirname + '/qgis2web_fiveThousand'));
->>>>>>> 96c478fcbced409d31dc112b8a15843456f79f8b
 app.use(router);
 
-app.listen(3000);
+app.listen(5000);
