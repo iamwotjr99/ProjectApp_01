1d5975b0d6bb0a64c777c6842cdf6a52a6b48827
iamwotjr99 <iamwotjr@gmail.com>
2021-09-29 22:11:45 +0900

    [수정중] body수정

--- RaoTalk/Server/app.js
+++ RaoTalk/Server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./router');
+const bodyParser = require('body-parser'); 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/qgis2web0', express.static(__dirname + '/qgis2web_total'));
 app.use('/qgis2web1', express.static(__dirname + '/qgis2web_oneThousand'));
 app.use('/qgis2web2', express.static(__dirname + '/qgis2web_twoThousand'));
