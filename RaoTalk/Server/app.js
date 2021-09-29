292bae80dba8c3493e0321972f0c89ce24f0d165
younglee-Choe <leele7111@naver.com>
2021-09-29 17:36:00 +0900

    Update app.js

--- RaoTalk/Server/app.js
+++ RaoTalk/Server/app.js
@@ -4,7 +4,11 @@ const router = require('./router');
 const app = express();
 
 app.use(morgan('dev'));
-app.use('/qgis2web', express.static(__dirname + '/qgis2web'));
+app.use('/qgis2web1', express.static(__dirname + '/qgis2web_oneThousand'));
+app.use('/qgis2web2', express.static(__dirname + '/qgis2web_twoThousand/index.html'));
+app.use('/qgis2web3', express.static(__dirname + '/qgis2web_threeThousand/index.html'));
+app.use('/qgis2web4', express.static(__dirname + '/qgis2web_fourThousand/index.html'));
+app.use('/qgis2web5', express.static(__dirname + '/qgis2web_fiveThousand/index.html'));
 app.use(router);
 
 app.listen(5000);
