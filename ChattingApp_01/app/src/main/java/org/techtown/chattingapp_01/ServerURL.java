5a201aa1578de353beff984392bcc3e15ca96c9d
iamwotjr99 <iamwotjr@gmail.com>
2021-09-29 23:33:19 +0900

    [수정중] 채팅 포트 수정

--- ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/ServerURL.java
+++ ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/ServerURL.java
@@ -1,6 +1,6 @@
 package org.techtown.chattingapp_01;
 
 public class ServerURL {
-    public static final String CHAT_SERVER_URL = "http://ec2-15-164-230-128.ap-northeast-2.compute.amazonaws.com:5000";
+    public static final String CHAT_SERVER_URL = "http://ec2-15-164-230-128.ap-northeast-2.compute.amazonaws.com:3000";
     public static final String RETROFIT_SERVER_URL = "http://ec2-15-164-230-128.ap-northeast-2.compute.amazonaws.com:5000";
 }
