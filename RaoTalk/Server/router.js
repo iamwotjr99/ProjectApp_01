3f50de4b0a946da3754719dce412dcd83b47dc83
iamwotjr99 <iamwotjr@gmail.com>
2021-09-29 21:48:04 +0900

    [수정]  스펠링 수정

--- RaoTalk/Server/router.js
+++ RaoTalk/Server/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const dpPool = require('./dbconfig');
+const dbPool = require('./dbconfig');
 
 router.get('/', serverTest);
 
